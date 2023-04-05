fc6d0ede6cbe1e81b54883d41432a13a16d7f984
shreyajain <shreyajain981@gmail.com>
2023-04-05 12:16:33 -0700

    now we can see user name on entering chatroom

--- frontend/src/components/Chatroom.js
+++ frontend/src/components/Chatroom.js
@@ -1,20 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Chatroom(){
+    const location = useLocation();
+    const user = location.state.user;
     return (
         <div>
-            
         <div className="chatroom">
-
         </div>
-
+        <p> Welcome {user}</p>
         <div className="message">
         <input type="text" placeholder="enter message" />
         <button>Enter</button>
         </div>
-
-
-
         </div>
     );
 }
