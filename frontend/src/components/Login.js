e5e16a9cd73413f1e849669a9cc0d1346f6fbac7
shreyajain <shreyajain981@gmail.com>
2023-04-04 18:25:31 -0700

    websocket working properly

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './Login.css';
 
-function Login(){
-    const [password, setRoomPassword] = useState('');
-    let navigate = useNavigate();
+function Login() {
+  const [password, setRoomPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-    const handlePassword = e => {
-        setRoomPassword(e.target.value);
+  const navigate = useNavigate();
+
+  const handlePassword = e => {
+    setRoomPassword(e.target.value);
+  }
+
+  const handleButton = e => {
+    e.preventDefault();
+    if (password.length === 0) {
+        setPasswordError('Password is required')
+    } else {
+        const webSocket = new WebSocket('ws://localhost:8080/ws');
+        webSocket.onopen = () => {
+        webSocket.send(JSON.stringify({ password }));
+        };
+  
+        webSocket.onmessage = (e) => {
+        const pass = JSON.parse(e.data);
+        if (pass.success) {
+            navigate("/chatroom");
+        } else {
+            setPasswordError('Invalid password');
+        }
+        webSocket.close();
+        }
+  
+        console.log(passwordError); // Log passwordError value here
     }
+  }
+  
 
-    const handleButton = e => {
-        e.preventDefault();
-        navigate("/chatroom");
-    }
-
-    return(
-        <div className = "login">
-            <br/>
-            <lable for = "code"> Enter Verification Code </lable>
-            <br/>
-            <br/>
-            <input type="text" placeholder="Room password" value = {password} onChange={handlePassword}/>
-            <br/>
-            <br/>
-            <button onClick={handleButton}> Join Room </button>
-        </div>
-    );
+  return (
+    <div className="login">
+      <br />
+      <label htmlFor="code"> Enter Verification Code </label>
+      <br />
+      <br />
+      <input type="text" placeholder="Room password" value={password} onChange={handlePassword} />
+      <div className="error">
+        {passwordError.length > 0 ? passwordError : ''}
+      </div>
+      <br />
+      <br />
+      <button onClick={handleButton}> Join Room </button>
+    </div>
+  );
 }
 
 export default Login;
